c69a953fa200fb21a49508db293988f9f5a2229d
DevExpressExampleBot <fakeEmail>
2018-12-21 03:22:54 +0300

    Source auto update [skip ci]

---
+++ VB/CustomTreeList-NodeHeight/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿Namespace CustomTreeList_NodeHeight
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.treeList1 = New CustomTreeList_NodeHeight.MyTreeList()
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' treeList1
+            ' 
+            Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.treeList1.Location = New System.Drawing.Point(0, 0)
+            Me.treeList1.Name = "treeList1"
+            Me.treeList1.Size = New System.Drawing.Size(489, 258)
+            Me.treeList1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(555, 258)
+            Me.Controls.Add(Me.treeList1)
+            Me.Name = "Form1"
+            Me.Text = "How to set individual node heights by creating a TreeList descendant"
+            Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+        #End Region
+
+        Private treeList1 As MyTreeList
+    End Class
+End Namespace
+
---
+++ VB/CustomTreeList-NodeHeight/MyTreeList.vb
@@ -0,0 +1,63 @@
+﻿Imports DevExpress.XtraTreeList
+Imports DevExpress.XtraTreeList.Nodes
+
+Namespace CustomTreeList_NodeHeight
+    Public Class MyTreeList
+        Inherits TreeList
+
+        Public Sub New()
+            MyBase.New()
+            OptionsBehavior.AutoNodeHeight = False
+        End Sub
+
+        Protected Overrides Function CreateNode(ByVal nodeID As Integer, ByVal owner As TreeListNodes, ByVal tag As Object) As TreeListNode
+            Return New MyTreeListNode(nodeID, owner)
+        End Function
+
+        Protected Overrides Sub InternalNodeChanged(ByVal node As TreeListNode, ByVal nodes As TreeListNodes, ByVal changeType As NodeChangeTypeEnum)
+            If changeType Is NodeChangeTypeEnum.User1 Then
+                LayoutChanged()
+            End If
+            MyBase.InternalNodeChanged(node, nodes, changeType)
+        End Sub
+        Protected Overrides Sub RaiseCalcNodeHeight(ByVal node As TreeListNode, ByRef nodeHeight As Integer)
+            Dim myNode As MyTreeListNode = TryCast(node, MyTreeListNode)
+            If myNode IsNot Nothing Then
+                nodeHeight = myNode.Height
+            Else
+                MyBase.RaiseCalcNodeHeight(node, nodeHeight)
+            End If
+        End Sub
+        Public Overridable ReadOnly Property DefaultNodesHeight() As Integer
+            Get
+                Return 18
+            End Get
+        End Property
+    End Class
+
+    Public Class MyTreeListNode
+        Inherits TreeListNode
+
+        Private Const minHeight As Integer = 5
+
+        Private height_Renamed As Integer
+        Public Sub New(ByVal id As Integer, ByVal owner As TreeListNodes)
+            MyBase.New(id, owner)
+            Me.height_Renamed = (TryCast(owner.TreeList, MyTreeList)).DefaultNodesHeight
+        End Sub
+        Public Property Height() As Integer
+            Get
+                Return height_Renamed
+            End Get
+            Set(ByVal value As Integer)
+                If Height = value OrElse value < minHeight Then
+                    Return
+                End If
+                height_Renamed = value
+                Changed(NodeChangeTypeEnum.User1)
+            End Set
+        End Property
+    End Class
+End Namespace
+
+
